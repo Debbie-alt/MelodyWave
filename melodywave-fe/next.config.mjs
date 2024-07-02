e89ce7f50f2050b683c8a0a231715a941ec1366f
Fayemi Funmilayo <busila976@gmail.com>
2024-07-02 14:04:13 +0100

    Add base path to config file

--- melodywave-fe/next.config.mjs
+++ melodywave-fe/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // basePath: "/melodywave-fe",
+  output: "export",  // <=== enables static exports
+
 };
 
 export default nextConfig;
