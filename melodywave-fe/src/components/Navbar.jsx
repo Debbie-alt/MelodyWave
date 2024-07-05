f0554890fb4c55b46aed55047fcc373e5c1210f4
Fayemi Funmilayo <busila976@gmail.com>
2024-07-05 12:06:25 +0100

    Add Custom Fonts

--- melodywave-fe/src/components/Navbar.jsx
+++ melodywave-fe/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-     <nav className=' p-10 sm:p-14 flex justify-between text-white font-serif'>
+     <nav className=' p-10 sm:p-14 flex justify-between text-white geist-regular'>
       <div className="flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-black before:to-purple-200 before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-purple-200 after:via-purple-400 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-purple-700/10 after:dark:from-purple-900 after:dark:via-[#7a0cc0]/40 before:lg:h-[360px]">
       <div className=' absolute flex  justify-between space-x-5  items-center '>
            <div>
