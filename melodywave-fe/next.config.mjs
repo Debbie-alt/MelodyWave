4395a571b8032fc7d9857f1303308d9ef574255d
Funmilayo Fayemi <busila976@gmail.com>
2024-07-10 11:31:41 +0100

    Setup Github publish workflow

--- melodywave-fe/next.config.mjs
+++ melodywave-fe/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basepath: "/melodywave-fe",
   output: "export", // Enables static exports
   images: {
     unoptimized: true,
