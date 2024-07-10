1c32a267017eada1872a4efd8e9a25127a49a8f5
Funmilayo Fayemi <busila976@gmail.com>
2024-07-10 12:13:53 +0100

    edit basepath

--- MelodyWave/next.config.mjs
+++ MelodyWave/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basepath: "/melodywave-fe",
+  basepath: "/MelodyWave",
   output: "export", // Enables static exports
   images: {
     unoptimized: true,
