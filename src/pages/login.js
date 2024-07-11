a04cdb1c9d38e2e46a02e569b8d8123c266fdfcf
Funmilayo Fayemi <busila976@gmail.com>
2024-07-11 15:30:42 +0100

    Run Np install and next build

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,8 +3,8 @@ export default function Login() {
         <>
             <main class=" bg-gradient-to-tr from-purple-400 to-[#040f0f]">
 
-                <section class="container flex flex-col items-center justify-center relative  h-screen">
-                    <h1 class="text-center text-white text-3xl mb-12  quicksand-bold -mt-2" >HELLO AGAIN!</h1>
+                <section className="container flex flex-col items-center justify-center relative  h-screen">
+                    <h1 className="text-center text-white text-3xl mb-12  quicksand-bold -mt-2" >HELLO AGAIN!</h1>
                     <figure className="  rounded-full" >
                         <div class="absolute left-[47%] lg:-mt-[2%] p-2 rounded-full bg-black log-icon">
                             <img width="58" height="58" src="https://img.icons8.com/external-isometric-vectorslab/68/external-Headphones-christmas-and-new-year-isometric-vectorslab.png" alt="external-Headphones-christmas-and-new-year-isometric-vectorslab" className=' w-[38px] h-[38px] sm:w-[58px] sm:h-[58px]' />
@@ -14,12 +14,12 @@ export default function Login() {
                                 Welcome back to Melodywave </h2>
                             <fieldset class=" space-y-6 w-3/5">
                                 <div class="relative w-full">
-                                    <img width="24" height="24" src="https://img.icons8.com/ios/50/username.png" alt="username" class="absolute mt-3 mx-3 mr-3" />
+                                    <img width="24" height="24" src="https://img.icons8.com/ios/50/username.png" alt="username" className="absolute mt-3 mx-3 mr-3" />
                                     <input type="text" placeholder="  Enter your email address" class="px-9 py-3 rounded-full bg-slate-200 w-full quicksand-reg" />
                                 </div>
 
-                                <div class="relative w-full">
-                                    <img width="24" height="24" src="https://img.icons8.com/cotton/64/lock--v3.png" alt="lock--v3" class="absolute mt-3 mx-3 mr-3" />
+                                <div className="relative w-full">
+                                    <img width="24" height="24" src="https://img.icons8.com/cotton/64/lock--v3.png" alt="lock--v3" className="absolute mt-3 mx-3 mr-3" />
                                     <input type="password" placeholder="  ••••••••••••" class="px-9 py-3 rounded-full bg-slate-200 w-full" />
                                 </div>
                             </fieldset>
