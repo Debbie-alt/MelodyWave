3ab801879d2db31d061736360d1cc24d81c59058
Debbie-alt <115513115+Debbie-alt@users.noreply.github.com>
2024-09-23 02:31:57 +0100

    disable ssr

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,27 @@
-import { Inter } from "next/font/google";
-import Navbar from "../components/Navbar";
-import { Header } from "../components/Header";
-import Features from "../components/Features";
-import About from "../components/About";
-import Footer from "../components/Footer";
- 
+'use client';
 
+import dynamic from 'next/dynamic';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
+
+// Dynamically import components with SSR disabled
+const Navbar = dynamic(() => import('../components/Navbar'), { ssr: false });
+const Header = dynamic(() => import('../components/Header'), { ssr: false });
+const Features = dynamic(() => import('../components/Features'), { ssr: false });
+const About = dynamic(() => import('../components/About'), { ssr: false });
+const Footer = dynamic(() => import('../components/Footer'), { ssr: false });
 
 export default function Home() {
   return (
     <main
-      className={` w-screen relative flex min-h-screen flex-col 
-      bg-gradient-to-tr from-purple-500 via-black to-[#040f0f] ${inter.className} overflow-x-hidden`}>
-      <Navbar/>
-      <Header/>
-      <Features/>
-       <About/>
-       <Footer/>
-  </main>
+      className={`w-screen relative flex min-h-screen flex-col bg-gradient-to-tr from-purple-500 via-black to-[#040f0f] ${inter.className} overflow-x-hidden`}
+    >
+      <Navbar />
+      <Header />
+      <Features />
+      <About />
+      <Footer />
+    </main>
   );
 }
